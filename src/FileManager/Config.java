e79ed4cfa8c28f680caa5052b7e0d507dc88ee39
vriera <vrieratorraca@gmail.com>
2022-05-12 18:57:43 -0300

    Config

--- src/FileManager/Config.java
+++ src/FileManager/Config.java
@@ -1,4 +1,120 @@
 package FileManager;
 
+import Integrations.*;
+import Main.Vector2D;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class Config {
+
+    public static double K , B , MASS , A , X0 , TOTAL_T , DELTA_T;
+    public static double Q , D , MIN_V0 , MAX_V0;
+    public static int DELTA_T2_FACTOR , N ;
+    private static final String config_path = "./config.json";
+    private static String INTEGRATION;
+    public static int SIM;
+    /*
+    double k = 1E10;
+    double q = 1E-19;
+    double m = 1E-27;
+    double d = 1E-8;
+    Vector2D v0 = new Vector2D( 5E3 ,0);
+    double minV0 = 5E4;
+    double maxV0 = 5E4;
+    double total_t = 5;
+    int delta_to_print = 100;
+    double delta_t = 1E-16;
+    int n = 16;
+    */
+
+    public static void readConfig(){
+        StringBuilder str_builder = new StringBuilder();
+        try {
+            Stream<String> stream = Files.lines(Paths.get(Config.config_path));
+            stream.forEach(s -> str_builder.append(s).append('\n'));
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        String content = str_builder.toString();
+        JSONObject root = new JSONObject(content);
+        SIM = root.getInt("simulation");
+        if(SIM == 0 ){
+            readConfigOscillation(root);
+        }else{
+            readConfigRadiation(root);
+        }
+    }
+
+
+        /*
+        double delta_t = 1E-3;
+        double k = 10000;
+        double b = 100;
+        double mass = 70;
+        double A = 1;
+        double x0 = 1;
+        double total_t = 5;
+        int delta_to_print = 1;
+        */
+
+    public static void readConfigOscillation(JSONObject o){
+        JSONObject sim = o.getJSONObject("config");
+        DELTA_T = sim.getDouble("delta_t");
+        TOTAL_T = sim.getDouble("total_t");
+        DELTA_T2_FACTOR = sim.getInt("delta_t2_factor");
+        K = sim.getDouble("k");
+        MASS = sim.getDouble("m");
+        A = sim.getDouble("a");
+        X0 = sim.getDouble("x0");
+        B = sim.getDouble("b");
+        INTEGRATION = sim.getString("integration");
+    }
+
+
+    public static Integration getIntegration(){
+        switch (INTEGRATION) {
+            case "beeman":
+                if(SIM == 1)
+                    System.out.println("Beeman is aproximating for forces affected by velocity");
+                return new Beeman();
+            case "gear":
+                if(SIM == 1)
+                    return new GearNoVel();
+                return new Gear();
+            default:
+                return new Verlet();
+        }
+
+    }
+    /*
+   double k = 1E10;
+   double q = 1E-19;
+   double m = 1E-27;
+   double d = 1E-8;
+   Vector2D v0 = new Vector2D( 5E3 ,0);
+   double minV0 = 5E4;
+   double maxV0 = 5E4;
+   double total_t = 5;
+   int delta_to_print = 100;
+   double delta_t = 1E-16;
+   int n = 16;
+   */
+    public static void  readConfigRadiation(JSONObject o){
+        JSONObject sim = o.getJSONObject("config");
+        DELTA_T = sim.getDouble("delta_t");
+        TOTAL_T = sim.getDouble("total_t");
+        DELTA_T2_FACTOR = sim.getInt("delta_t2_factor");
+        K = sim.getDouble("k");
+        MASS = sim.getDouble("m");
+        Q = sim.getDouble("q");
+        D = sim.getDouble("d");
+        N = sim.getInt("n");
+        MIN_V0 = sim.getDouble("min_v0");
+        MAX_V0 = sim.getDouble("max_v0");
+        INTEGRATION = sim.getString("integration");
+    }
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,11 +1,16 @@
 package Main;
 
+<<<<<<< Updated upstream
 import FileManager.OscillationOutputManager;
+=======
+import FileManager.Config;
+>>>>>>> Stashed changes
 import FileManager.OutputManager;
 import Integrations.*;
 
 public class Main {
     public static void main(String[] args) {
+<<<<<<< Updated upstream
 
         double delta_t = 1E-3;
         double k = 10000;
@@ -32,6 +37,35 @@ public class Main {
 //        double delta_t = 1E-16;
 //        int n = 16;
 //        Radiation.run(delta_to_print, total_t, delta_t, n, d, q, m, k, minV0 , maxV0, new GearNoVel(), null, new OutputManager(null));
+=======
+        Config.readConfig();
+        if(Config.SIM == 0 ) {
+            double delta_t = 1E-3;
+            double k = 10000;
+            double b = 100;
+            double mass = 70;
+            double A = 1;
+            double x0 = 1;
+            double total_t = 5;
+            int delta_to_print = 1;
+            Oscillation.run(Config.DELTA_T2_FACTOR, Config.TOTAL_T, Config.DELTA_T, Config.K, Config.B, Config.A, Config.X0, Config.MASS, Config.getIntegration());
+            return;
+        }
+
+        double k = 1E10;
+        double q = 1E-19;
+        double m = 1E-27;
+        double d = 1E-8;
+        Vector2D v0 = new Vector2D( 5E3 ,0);
+        double minV0 = 5E4;
+        double maxV0 = 5E4;
+        double total_t = 5;
+        int delta_to_print = 100;
+        double delta_t = 1E-16;
+        int n = 16;
+        Radiation.run(Config.DELTA_T2_FACTOR, Config.TOTAL_T, Config.DELTA_T, Config.N,
+                Config.D, Config.Q, Config.MASS, Config.K, Config.MIN_V0 ,Config.MAX_V0, Config.getIntegration(), null, new OutputManager(null));
+>>>>>>> Stashed changes
 
     }
 
